078c405dac5fba3fd8732f36289e4bb39a65030a
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2019-09-07 22:56:33 +0700

    formatting fixed

--- signature-hmac-example.js
+++ signature-hmac-example.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 const header = { "payloadVersion": 2, "signatureVersion" : 1 };
-const APP_SECRET	 = "a751abdb-e260-4bfd-a42c-60660561123d-3d8e6a30-0f39-42f0-a1ec-e47d47fb1392";
+const APP_SECRET  = "a751abdb-e260-4bfd-a42c-60660561123d-3d8e6a30-0f39-42f0-a1ec-e47d47fb1392";
 
 function getSignature(message, appsecert) {
     return crypto.createHmac('sha256', appsecert).update(message).digest('base64');
@@ -31,9 +31,12 @@ let event = {
 console.log(event)
 
 /*
-{ header: { payloadVersion: 2, signatureVersion: 1 },
-  payload:
-   { replyToken: '6ec1f778-e92f-487c-9818-bdbe3438f30e',
+{ header: { 
+   payloadVersion: 2, 
+   signatureVersion: 1 
+  },
+  payload: { 
+     replyToken: '6ec1f778-e92f-487c-9818-bdbe3438f30e',
      clientId: 'alexa-skill',
      createdAt: 1567852244,
      deviceId: '5d737888aea17c30a056d759',
@@ -41,5 +44,7 @@ console.log(event)
      type: 'request',
      action: 'setPowerState',
      value: { state: 'On' } },
-  signature: { HMAC: 'jjrxa5b7fzXXml2+PxlfUYIUgLdHg33Cr2yWVuzlr/s=' } }
+  signature: { 
+     HMAC: 'jjrxa5b7fzXXml2+PxlfUYIUgLdHg33Cr2yWVuzlr/s=' } 
+  }
 */

abc4308230f0565d6951b690c410294f1bc0ee2e
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2019-09-17 10:13:12 +0700

    json format updated
    json format updated

--- signature-hmac-example.js
+++ signature-hmac-example.js
@@ -4,10 +4,10 @@ const APP_SECRET  = "a751abdb-e260-4bfd-a42c-60660561123d-3d8e6a30-0f39-42f0-a1e
 function getSignature(message, appsecert) {
     return crypto.createHmac('sha256', appsecert).update(message).digest('base64');
 }
-	
-const payload = '{"replyToken":"6ec1f778-e92f-487c-9818-bdbe3438f30e","clientId":"alexa-skill","createdAt":1567852244,"deviceId":"5d737888aea17c30a056d759","deviceAttributes":[],"type":"request","action":"setPowerState","value":{"state":"On"}}';
 
-const HMAC = getSignature(JSON.stringify(payload), APP_SECRET);
+// Remove new line breaks and spaces 
+const payload = '{"replyToken":"6ec1f778-e92f-487c-9818-bdbe3438f30e","clientId":"alexa-skill","createdAt":1567852244,"deviceId":"5d737888aea17c30a056d759","deviceAttributes":[],"type":"request","action":"setPowerState","value":{"state":"On"}}';
+const HMAC = getSignature(payload, APP_SECRET);
 const signature = { "HMAC": HMAC };
 const header = { "payloadVersion": 2, "signatureVersion" : 1 };
 

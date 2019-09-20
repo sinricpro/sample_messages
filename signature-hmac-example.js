a0c3b8a0c9670e316af6d43c707f6b580d687585
Aruna Tennakoon <aruna.tennakoon@gmail.com>
2019-09-20 13:39:44 +0700

    comments updated

--- signature-hmac-example.js
+++ signature-hmac-example.js
@@ -5,7 +5,7 @@ function getSignature(message, appsecert) {
     return crypto.createHmac('sha256', appsecert).update(message).digest('base64');
 }
 
-// Remove new line breaks and spaces 
+// Remove new line breaks and spaces and sort by key 
 const payload = {action:"setPowerState",clientId:"alexa-skill",createdAt:1567852244,deviceAttributes:[],deviceId:"5d737888aea17c30a056d759",replyToken:"6ec1f778-e92f-487c-9818-bdbe3438f30e",type:"request",value:{state:"On"}}
 const HMAC = getSignature(JSON.stringify(payload), APP_SECRET);
 const signature = { "HMAC": HMAC };
